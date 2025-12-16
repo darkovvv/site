2c4dc81449af4e4d3350e8ef9681fea2b7f8261c
darkovvv <136109733+darkovvv@users.noreply.github.com>
2025-12-17 02:06:06 +0300

    Add files via upload

---
+++ frontend/app/services/page.jsx
@@ -0,0 +1,19 @@
+export default function ServicesPage() {
+  return (
+    <section>
+      <h2>Услуги</h2>
+
+      <div>
+        <h4>Установка розетки</h4>
+        <p>Электромонтажные работы</p>
+        <button>В корзину</button>
+      </div>
+
+      <div>
+        <h4>Ремонт смесителя</h4>
+        <p>Сантехнические работы</p>
+        <button>В корзину</button>
+      </div>
+    </section>
+  );
+}
